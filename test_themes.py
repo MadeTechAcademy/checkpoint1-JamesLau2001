0b4e13f285d4f6b99b38d0e3b3dd6adf331c8218
JamesLau2001 <james_lau2001@outlook.com>
2025-07-09 15:32:18 +0100

    added back missing work

--- test_themes.py
+++ test_themes.py
@@ -1,5 +1,5 @@
-from themes import duties, display_duties
 from utility_functions.write_html import write_html
+from themes import duties, display_duties
 
 def test_amount_of_duties_is_correct():
     assert len(duties) == 13
